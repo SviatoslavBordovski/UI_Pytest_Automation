3b3646a205b85c48b81d14e24dfb19f697958a58
Sviatoslav Bordovski <sbordovski@gmail.com>
2021-06-03 23:06:59 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,23 +1,25 @@
 import pytest
 from selenium import webdriver
-#from webdriver_manager.chrome import ChromeDriverManager
-#from webdriver_manager.firefox import GeckoDriverManager
-#from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="Type in browser Chrome or Firefox")
 
 @pytest.fixture(scope="class")
 def test_setup(request):
-    from selenium import webdriver
     browser = request.config.getoption("--browser")
 
     if browser == "chrome":
-        #driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver = webdriver.Chrome(executable_path="drivers/chromedriver")
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        #driver = webdriver.Chrome(executable_path="drivers/chromedriver")
     elif browser == "firefox":
-        #driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-        driver = webdriver.Firefox(executable_path="/drivers/geckodriver")
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        #driver = webdriver.Firefox(executable_path="/drivers/geckodriver")
     elif browser == "opera":
         driver = webdriver.Opera(executable_path="/drivers/operadriver")
     else:
