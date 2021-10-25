f0dd1d38c4491f426651163fe75876b59d06df04
Sviatoslav Bordovski <sbordovski@gmail.com>
2021-10-25 13:28:36 +0300

    Update 2_addNewNotExistingUser_test.py

--- tests/2_addNewNotExistingUser_test.py
+++ tests/2_addNewNotExistingUser_test.py
@@ -37,7 +37,7 @@ class TestAdminPage:
         adminpage.verify_saved_user()
 
     def test_search_created_user(self):
-        """Finding newly added user"""
+        """Finding newly created user"""
         driver = self.driver
         adminpage = AdminPage(driver)
         adminpage.search_new_saved_user(utils.newUSERNAME)
