0d11087da2bef202f76c216f320fb1eecca324d1
Sviatoslav Bordovski <sbordovski@gmail.com>
2021-11-09 21:42:40 +0200

    Update homePage.py

--- pages/homePage.py
+++ pages/homePage.py
@@ -3,7 +3,6 @@ class HomePage:
     def __init__(self, driver):
         """Constructor for initializing all attributes of the class"""
         self.driver = driver
-
         self.welcome_link_id = "welcome"
         self.logout_link_linkText = "Logout"
 
