c5ca2a1740fc178b3cd75e2ad33dab5011555e8d
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-01-31 21:40:31 +0200

    Update loginPage.py

--- pages/loginPage.py
+++ pages/loginPage.py
@@ -1,19 +1,27 @@
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
 class LoginPage:
 
     def __init__(self, driver):
-        """Constructor for initializing all the attributes of class"""
         self.driver = driver
+
         self.username_textbox_id = "txtUsername"
         self.password_textbox_id = "txtPassword"
         self.login_button_id = "btnLogin"
+        self.login_header_id = "logInPanelHeading"
 
     def enter_username(self, username):
-        self.driver.find_element_by_id(self.username_textbox_id).clear()
-        self.driver.find_element_by_id(self.username_textbox_id).send_keys(username)
+        self.driver.find_element(By.ID, self.username_textbox_id).send_keys(Keys.CONTROL + 'a', Keys.DELETE)
+        self.driver.find_element(By.ID, self.username_textbox_id).send_keys(username)
 
     def enter_password(self, password):
-        self.driver.find_element_by_id(self.password_textbox_id).clear()
-        self.driver.find_element_by_id(self.password_textbox_id).send_keys(password)
+        self.driver.find_element(By.ID, self.password_textbox_id).send_keys(Keys.CONTROL + 'a', Keys.DELETE)
+        self.driver.find_element(By.ID, self.password_textbox_id).send_keys(password)
 
     def click_login_button(self):
-        self.driver.find_element_by_id(self.login_button_id).click()
+        self.driver.find_element(By.ID, self.login_button_id).click()
+
+    def panel_header_check(self):
+        title = self.driver.find_element(By.ID, self.login_header_id).text
+        assert "LOGIN Panel" == title
