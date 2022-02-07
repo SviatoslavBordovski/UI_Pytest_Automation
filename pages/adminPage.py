04c4b12cec68dd33ea2bca7ef3d3f962258ae04f
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-02-07 20:52:40 +0200

    Update adminPage.py

--- pages/adminPage.py
+++ pages/adminPage.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 
 class AdminPage:
 
