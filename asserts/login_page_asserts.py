8d14eba34a37e90539e08277157f8203dec6741b
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-02-16 21:34:24 +0200

    Create login_page_asserts.py

---
+++ asserts/login_page_asserts.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from utils import utils as utils
+
+class LoginAsserts:
+    def __init__(self, driver):
+        self.driver = driver
+        self.login_logo_image_id = 'divLogo'
+        self.forgot_password_link_id = 'forgotPasswordLink'
+
+    def assert_login_page_contains_logo_image(self):
+        visible_logo = self.driver.find_element(By.ID, self.login_logo_image_id).is_displayed()
+        assert visible_logo is True
+
+    def assert_forgot_password_link_displayed(self):
+        password_link_locator = self.driver.find_element(By.ID, self.forgot_password_link_id)
+        assert password_link_locator.is_displayed()
+        assert password_link_locator.text == utils.forgotPassword
