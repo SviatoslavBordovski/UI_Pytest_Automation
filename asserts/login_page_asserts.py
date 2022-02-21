1f7298a2ac4385296b6c23678b0d1c407730c9b6
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-02-21 22:12:39 +0200

    Update login_page_asserts.py

--- asserts/login_page_asserts.py
+++ asserts/login_page_asserts.py
@@ -6,12 +6,18 @@ class LoginAsserts:
         self.driver = driver
         self.login_logo_image_id = 'divLogo'
         self.forgot_password_link_id = 'forgotPasswordLink'
+        self.login_header_id = "logInPanelHeading"
 
     def assert_login_page_contains_logo_image(self):
         visible_logo = self.driver.find_element(By.ID, self.login_logo_image_id).is_displayed()
         assert visible_logo is True
 
+    def panel_header_check(self):
+        title = self.driver.find_element(By.ID, self.login_header_id).text
+        assert "LOGIN Panel" == title
+
     def assert_forgot_password_link_displayed(self):
         password_link_locator = self.driver.find_element(By.ID, self.forgot_password_link_id)
         assert password_link_locator.is_displayed()
         assert password_link_locator.text == utils.forgotPassword
+ 
