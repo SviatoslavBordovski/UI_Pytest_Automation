b79f15e48977f1c4d22d65e731984fa584137a53
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-02-21 17:09:36 +0200

    Update loginPage.py

--- pages/loginPage.py
+++ pages/loginPage.py
@@ -1,5 +1,6 @@
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
+import utils.utils
 
 class LoginPage:
 
@@ -10,6 +11,8 @@ class LoginPage:
         self.password_textbox_id = "txtPassword"
         self.login_button_id = "btnLogin"
         self.login_header_id = "logInPanelHeading"
+        self.invalid_credentials_flag_id = "spanMessage"
+        self.forgot_password_linktext = "Forgot your password?"
 
     def enter_username(self, username):
         self.driver.find_element(By.ID, self.username_textbox_id).send_keys(Keys.CONTROL + 'a', Keys.DELETE)
@@ -25,3 +28,11 @@ class LoginPage:
     def panel_header_check(self):
         title = self.driver.find_element(By.ID, self.login_header_id).text
         assert "LOGIN Panel" == title
+
+    def shown_invalid_credentials_flag(self):
+        invalid_credentials_flag = self.driver.find_element(By.ID, self.invalid_credentials_flag_id).text
+        assert invalid_credentials_flag == utils.utils.expectedInvalidCredentialsFlag
+
+    def click_forgot_password_link(self):
+        forgot_password_link = self.driver.find_element(By.LINK_TEXT, self.forgot_password_linktext).click()
+        forgot_password_link.is_selected()
