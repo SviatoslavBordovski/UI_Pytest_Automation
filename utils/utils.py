1bf1fa155d573a91b57a01bc53db46ba6432c37e
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-02-21 17:06:58 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -8,6 +8,8 @@ employeeNAME = "Charlie Carter"
 newUSERNAME = "2021"
 strongPASSWORD = "q1w2e3r4t5y6u7i8o9!~!"
 forgotPassword = "Forgot your password?"
+expectedInvalidCredentialsUrl = "https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials"
+expectedInvalidCredentialsFlag = "Invalid credentials"
 
 def whoami():
     return inspect.stack()[1][3]
