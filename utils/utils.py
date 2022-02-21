f77f4ca882ffe2423e13bc35ea38de31cced0d6f
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-02-21 22:16:28 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -10,6 +10,8 @@ strongPASSWORD = "q1w2e3r4t5y6u7i8o9!~!"
 forgotPassword = "Forgot your password?"
 expectedInvalidCredentialsUrl = "https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials"
 expectedInvalidCredentialsFlag = "Invalid credentials"
+expectedEmptyCredentialsFlag = "Username cannot be empty"
+forgotPasswordFormUrl = "https://opensource-demo.orangehrmlive.com/index.php/auth/requestPasswordResetCode"
 
 def whoami():
     return inspect.stack()[1][3]
