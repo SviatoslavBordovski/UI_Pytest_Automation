2ae5077e2780f2c92d5f34f5c26bdf6b1a62f31a
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-04-25 11:00:32 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -10,7 +10,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.edge.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
-from webdriver_manager.opera import OperaDriverManager
 from asserts.login_page_asserts import LoginAsserts
 from pages.loginPage import LoginPage
 from pages.homePage import HomePage
@@ -18,50 +17,86 @@ from utils import utils
 from pytest_html_reporter import attach
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="chrome", help="Type in CLI --browser=Chrome or Firefox")
+    parser.addoption("--browser", action="store", default="chrome",
+                     help="Type in CLI --desktop-browser=Chrome or Firefox")
+    parser.addoption('--host', default='staging',
+                     help='host options: "staging", "production", or your own host for local testing')
+    parser.addoption('--headless', default='true',
+                     help='headless options: "true" or "false"')
+    parser.addoption('--mobile-browser', default='brave',
+                     help='option to define web or mobile browser')
 
 @pytest.fixture(scope="class")
 def login_test_setup(request):
+    """
+    Headless mode (default='true'): pytest -m tc2 --browser=chrome --headless=false
+    Mobile/tablet screen size: pytest -m tc2 --browser=firefox --mobile-browser=firefox
+    Desktop mode (default='chrome'): pytest -m tc2 --browser=firefox
+    """
     global driver
     browser = request.config.getoption("--browser")
+    headless = request.config.getoption("--headless")
+    mobile_browser = request.config.getoption("--mobile-browser")
 
     if browser == "chrome":
         from selenium.webdriver.chrome.options import Options
         ch = Service(ChromeDriverManager().install())
         chrome_options = Options()
-        chrome_options.add_argument('--headless')
+
+        if headless == "true":  # usage => pytest -m tc2 --browser=chrome --headless=true
+            chrome_options.add_argument('--headless')  # type in CLI '--headless=false' to run test with visible UI
+            logger.info(">>> Headless mode turned on <<<")
+
         driver = webdriver.Chrome(service=ch, options=chrome_options)
-        logger.info("Chrome tests run has started")
-        # driver = webdriver.Chrome(executable_path=r"path_to_driver")
+        driver.maximize_window()
+
+        if mobile_browser == "chrome":
+            driver.set_window_size(960, 640)
+            size = driver.get_window_size()
+            logger.info("Window size: width = {0}px, height = {1}px".format(size["width"], size["height"]))
+            logger.info(">>> Tablet Chrome browser size set in CLI <<<")
+
     elif browser == "firefox":
         from selenium.webdriver.firefox.options import Options
         ff = Service(GeckoDriverManager().install())
         ff_options = Options()
-        ff_options.add_argument('--headless')
+
+        if headless == "true":
+            ff_options.add_argument('--headless')
+            logger.info(">>> Headless mode turned on <<<")
+
         driver = webdriver.Firefox(service=ff, options=ff_options)
-        # driver = webdriver.Firefox(GeckoDriverManager().install(), options=ff_options)
-        logger.info("Firefox tests run has started")
-        # driver = webdriver.Firefox(executable_path=r"path_to_driver")
+        driver.maximize_window()
+
+        # add both options => 'pytest -m tc2 --browser=firefox --mobile-browser=firefox'
+        if mobile_browser == "firefox":
+            driver.set_window_size(960, 640)
+            size = driver.get_window_size()
+            logger.info("Window size: width = {0}px, height = {1}px".format(size["width"], size["height"]))
+            logger.info(">>> Tablet Firefox browser size set in CLI <<<")
+
     elif browser == "edge":
         from selenium.webdriver.edge.options import Options
         ed = Service(EdgeChromiumDriverManager(log_level=20).install())
         edge_options = Options()
-        edge_options.add_argument('--headless')
+
+        if headless == "true":
+            edge_options.add_argument('--headless')
+            logger.info(">>> Headless mode turned on <<<")
+
         driver = webdriver.Edge(service=ed, options=edge_options)
-        # driver = webdriver.Edge(executable_path=r"path_to_driver")
-        logger.info("Edge tests run has started")
-    # elif browser == "opera":
-        # options = Options()
-        # options.add_argument('allow-elevated-browser')
-        # options.binary_location = "C:\\Users\\sbord_iu0ld13\\AppData\\Local\\Programs\\Opera\\opera.exe"
-        # driver = webdriver.Opera(executable_path=OperaDriverManager().install())
-        # logger.info("Opera tests run has started")
-        # driver = webdriver.Opera(executable_path="/home/sviatoslav/PytestFrameworkProject/drivers/operadriver")
+        driver.maximize_window()
+
+        if mobile_browser == "edge":
+            driver.set_window_size(960, 640)
+            size = driver.get_window_size()
+            logger.info("Window size: width = {0}px, height = {1}px".format(size["width"], size["height"]))
+            logger.info(">>> Tablet Edge browser size set in CLI <<<")
+
     else:
         logger.info("Such browser is not supported, please contact QA Automation Team to learn more about the issue")
 
-    driver.implicitly_wait(10)
-    driver.maximize_window()
+    driver.implicitly_wait(30)
     request.cls.driver = driver
     yield
     driver.close()
@@ -81,7 +116,6 @@ def standard_test_setup_teardown(request):
         chrome_options.add_argument('--headless')
         driver = webdriver.Chrome(service=ch, options=chrome_options)
         logger.info("Chrome tests run has started")
-        # driver = webdriver.Chrome(executable_path=r"path_to_driver")
     elif browser == "firefox":
         from selenium.webdriver.firefox.options import Options
         ff = Service(GeckoDriverManager().install())
@@ -89,32 +123,33 @@ def standard_test_setup_teardown(request):
         ff_options.add_argument('--headless')
         driver = webdriver.Firefox(service=ff, options=ff_options)
         logger.info("Firefox tests run has started")
-        # driver = webdriver.Firefox(executable_path=r"path_to_driver")
-        
     elif browser == "edge":
         from selenium.webdriver.edge.options import Options
         ed = Service(EdgeChromiumDriverManager(log_level=20).install())
         edge_options = Options()
         edge_options.add_argument('--headless')
         driver = webdriver.Edge(service=ed, options=edge_options)
-        # driver = webdriver.Edge(executable_path=r"path_to_driver")
         logger.info("Edge tests run has started")
-        
     else:
         logger.info("Such browser is not supported, please contact QA Automation Team to learn more about the issue")
 
     driver.get(utils.URL)
-    logger.info("Requested website has been opened")
-    actual_login_page_title = driver.title
-    assert actual_login_page_title == 'OrangeHRM'
-
     login_page = LoginPage(driver)
+    login_page_asserts = LoginAsserts(driver)
+    logger.info("Website has been opened")
+    assert driver.title == 'OrangeHRM', f"ERROR! Actual title => {driver.title}"
+
+    # Check if panel header, logo image and 'forgot password' link are present
+    login_page_asserts.panel_header_check()
+    login_page_asserts.assert_login_page_contains_logo_image()
+    login_page_asserts.assert_forgot_password_link_displayed()
+
     login_page.enter_username(utils.USERNAME)
     login_page.enter_password(utils.PASSWORD)
     login_page.click_login_button()
     logger.info("User logged in")
 
-    driver.implicitly_wait(30)
+    driver.implicitly_wait(10)
     driver.maximize_window()
     request.cls.driver = driver
 
@@ -140,7 +175,7 @@ def standard_test_setup_teardown(request):
         print(error)
         attach(data=driver.get_screenshot_as_png())
         raise
-   
+
     else:
         logger.info("Teardown point reached")
         attach(data=driver.get_screenshot_as_png())
