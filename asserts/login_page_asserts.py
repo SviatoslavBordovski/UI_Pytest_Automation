d9d55035ac0048bfd29b03fcd1c8e3b54799433e
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-04-28 09:16:28 +0300

    Update login_page_asserts.py

--- asserts/login_page_asserts.py
+++ asserts/login_page_asserts.py
@@ -10,14 +10,15 @@ class LoginAsserts:
 
     def assert_login_page_contains_logo_image(self):
         visible_logo = self.driver.find_element(By.ID, self.login_logo_image_id).is_displayed()
-        assert visible_logo is True
+        assert visible_logo is True, f"Real found boolean value for logo is {visible_logo}"
 
     def panel_header_check(self):
         title = self.driver.find_element(By.ID, self.login_header_id).text
-        assert "LOGIN Panel" == title
+        assert "LOGIN Panel" == title, f"ERROR! Found title => {title}"
 
     def assert_forgot_password_link_displayed(self):
         password_link_locator = self.driver.find_element(By.ID, self.forgot_password_link_id)
-        assert password_link_locator.is_displayed()
-        assert password_link_locator.text == utils.forgotPassword
- 
+        assert password_link_locator.is_displayed(), f"Real found boolean for PW link locator " \
+                                                     f"is {password_link_locator.is_displayed()}"
+        assert password_link_locator.text == utils.forgotPassword, f"Password link text is not " \
+                                                                   f"matching {password_link_locator.text}"
