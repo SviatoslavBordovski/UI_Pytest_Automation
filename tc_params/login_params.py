85865aed58b875d4cad5f0ed843e3e6d6cdea279
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-05-01 17:41:44 +0300

    Create login_params.py

---
+++ tc_params/login_params.py
@@ -0,0 +1,18 @@
+import pytest
+
+def first_tc_login_params():
+    list = [
+        ("Admin", "admin123"),
+        pytest.param("random_username", "admin123",
+                     marks=pytest.mark.xfail(reason="Non-existing username entered")),
+        pytest.param("Admin", "qwerty123!",
+                     marks=pytest.mark.xfail(reason="Incorrect password entered")),
+        pytest.param("qwertyADMIN", "qwertyADMIN",
+                     marks=pytest.mark.xfail(reason="Wrong credentials")),
+        pytest.param(" ", "admin123",
+                     marks=pytest.mark.xfail(reason="Empty username")),
+        pytest.param("Admin", " ",
+                     marks=pytest.mark.xfail(reason="Empty password"))
+    ]
+
+    return list
