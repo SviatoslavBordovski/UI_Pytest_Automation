7fa585df5523455361acc4a0fe8f729f8f4fc68c
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-05-02 10:21:01 +0300

    Update 3_addNewNotExistingUser_test.py

--- tests/3_addNewNotExistingUser_test.py
+++ tests/3_addNewNotExistingUser_test.py
@@ -1,22 +1,22 @@
 import logging as logger
-import time
 import pytest
 import selenium
-import utils.utils as utils
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
 from pages.adminPage import AdminPage
+from tc_params import new_not_existing_user_params
+from selenium.common.exceptions import NoSuchElementException
 
 @pytest.mark.usefixtures("standard_test_setup_teardown")
 @pytest.mark.tc3
-@pytest.mark.regression
 class TestAdminPage:
 
-    def test_addUser(self):  # PARAMETRIZE THIS TEST !!!
+    tc_three_params = new_not_existing_user_params.third_tc_new_user_params()
+
+    @pytest.mark.parametrize("employee_name, new_password, confirm_password", tc_three_params)
+    def test_addUser(self, employee_name, new_password, confirm_password):
         """Adding not existing user"""
         try:
             adminpage = AdminPage(self.driver)
-            adminpage.add_new_user(utils.employeeNAME)
+            adminpage.add_new_user(employee_name, new_password, confirm_password)
 
         except AttributeError as error:  # refactor exceptions https://youtu.be/KX5miQRROJU
             logger.error("Locator issue")
