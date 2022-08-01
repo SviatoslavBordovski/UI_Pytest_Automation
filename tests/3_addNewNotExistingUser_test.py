32df3765dd120b0f5d3c6a3e653f0aad923980b9
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-08-01 22:30:38 +0300

    Update 3_addNewNotExistingUser_test.py

--- tests/3_addNewNotExistingUser_test.py
+++ tests/3_addNewNotExistingUser_test.py
@@ -1,16 +1,15 @@
 import logging as logger
 import pytest
-import selenium
 from pages.adminPage import AdminPage
 from tc_params import new_not_existing_user_params
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 
 @pytest.mark.usefixtures("standard_test_setup_teardown")
-@pytest.mark.tc3
-class TestAdminPage:
+class TestAdminPage():
 
     tc_three_params = new_not_existing_user_params.third_tc_new_user_params()
 
+    @pytest.mark.tc3
     @pytest.mark.parametrize("employee_name, new_password, confirm_password", tc_three_params)
     def test_addUser(self, employee_name, new_password, confirm_password):
         """Adding not existing user"""
@@ -18,13 +17,9 @@ class TestAdminPage:
             adminpage = AdminPage(self.driver)
             adminpage.add_new_user(employee_name, new_password, confirm_password)
 
-        except AttributeError as error:  # refactor exceptions https://youtu.be/KX5miQRROJU
-            logger.error("Locator issue")
-            raise error
-
-        except selenium.common.exceptions.NoSuchElementException as error:
-            logger.error("Locator was not shown or found by driver")
-            raise error
+        except (NoSuchElementException, AttributeError, StaleElementReferenceException) as e:
+            logger.error("Locator issue, maybe it was not shown or found by driver")
+            raise e
 
         else:
             logger.info("In case if something mysterious happens => please check logs in CLI")
