44092b08f7d572af1098bfdda39ce90ab02bafd2
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-08-01 23:01:02 +0300

    Update 1_login_test.py

--- tests/1_login_test.py
+++ tests/1_login_test.py
@@ -1,7 +1,6 @@
 import time
 import pytest
-import selenium
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from asserts.login_page_asserts import LoginAsserts
 from pages.loginPage import LoginPage
 from pages.homePage import HomePage
@@ -10,48 +9,43 @@ from tc_params import login_params
 import logging as logger
 
 @pytest.mark.usefixtures("login_test_setup")
-@pytest.mark.tc1
 class TestLoginLogout:
 
-    @pytest.mark.parametrize("username, password", login_params.first_tc_login_params())
+    tc_one_params = login_params.first_tc_login_params()
+
+    @pytest.mark.tc1
+    @pytest.mark.parametrize("username, password", tc_one_params)
     def test_login(self, username, password):
         """Sign in to the website"""
-        driver = self.driver
-        driver.get(utils.URL)
-        login = LoginPage(driver)
-        homepage = HomePage(driver)
-        login_page_asserts = LoginAsserts(driver)
 
-        time.sleep(3)
-        login_page_asserts.panel_header_check()
-        login_page_asserts.assert_login_page_contains_logo_image()
-        login_page_asserts.assert_forgot_password_link_displayed()
-
-        login.login_crm_system(username, password)
-        login.click_login_button()
-        homepage.check_dashboard_visibility()
-
-    def test_logout(self):
-        """Sign out from the website"""
-        global driver
         try:
-            driver = self.driver  # defines the driver imported from conftest.py file
+            driver = self.driver
+            driver.get(utils.URL)
+            login = LoginPage(driver)
             homepage = HomePage(driver)
             login_page_asserts = LoginAsserts(driver)
 
+            # Check elements are shown
+            time.sleep(3)
+            login_page_asserts.panel_header_check()
+            login_page_asserts.assert_login_page_contains_logo_image()
+            login_page_asserts.assert_forgot_password_link_displayed()
+
+            # Sign in and check if user was successfully signed in
+            login.login_crm_system(username, password)
+            login.click_login_button()
+            homepage.check_dashboard_visibility()
+
+            # Sign out and check user was signed out
             homepage.click_welcome_button()
             homepage.click_logout_button()
             time.sleep(3)
             login_page_asserts.assert_login_page_contains_logo_image()
             login_page_asserts.assert_forgot_password_link_displayed()
 
-        except AttributeError as error:  # refactor exceptions https://youtu.be/KX5miQRROJU
-            logger.error("Locator issue")
-            raise error
-
-        except selenium.common.exceptions.NoSuchElementException as error:
-            logger.error("Locator was not shown or found by driver")
-            raise error
+        except (NoSuchElementException, AttributeError, StaleElementReferenceException) as e:
+            logger.error("Locator issue, maybe it was not shown or found by driver")
+            raise e
 
         else:
             logger.info("In case if something mysterious happens => please check logs in CLI")
