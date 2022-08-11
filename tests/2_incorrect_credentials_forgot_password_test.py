4dfff76c68894bfcdd69f0553882bfc6cb74af46
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-08-11 10:53:22 +0300

    Update 2_incorrect_credentials_forgot_password_test.py

--- tests/2_incorrect_credentials_forgot_password_test.py
+++ tests/2_incorrect_credentials_forgot_password_test.py
@@ -1,18 +1,16 @@
 import logging as logger
 import pytest
-import selenium
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from asserts.login_page_asserts import LoginAsserts
 from pages.loginPage import LoginPage
 from utils import utils as utils
 
 @pytest.mark.usefixtures("login_test_setup")
 @pytest.mark.tc2
-@pytest.mark.regression
 class TestForgotPassword:
 
     def test_forgot_password_feature(self):
-        """Sign in to the website"""
+        """Sign in with empty credentials, see the incorrect credentials label, cancel and fill forgot password form"""
         try:
             driver = self.driver
             driver.get(utils.URL)
@@ -24,18 +22,14 @@ class TestForgotPassword:
             login_page_asserts.assert_login_page_contains_logo_image()
             login_page_asserts.assert_forgot_password_link_displayed()
 
-            # Login with empty credentials, then login with wrong credentials and check 'forgot password' form works
+            # Login with empty credentials, then with wrong credentials and check 'forgot password' form works
             login_page.sign_in_with_empty_or_incorrect_credentials()
             login_page.cancel_fill_forgot_password_form(utils.forgotPasswordTitle)
             login_page.fill_forgot_password_form(utils.employeeNAME)
 
-        except AttributeError as error:  # refactor exceptions https://youtu.be/KX5miQRROJU
-            logger.error("Locator issue")
-            raise error
-
-        except selenium.common.exceptions.NoSuchElementException as error:
-            logger.error("Locator was not shown or found by driver")
-            raise error
+        except (NoSuchElementException, AttributeError, StaleElementReferenceException) as e:
+            logger.error("Locator issue, maybe it was not shown or found by driver")
+            raise e
 
         else:
             logger.info("In case if something mysterious happens => please check logs in CLI")
